1b85bb30776e8b294c4da7a26594f0ce7bc4a406
FatimaCapilla <fatima.capilla.sancho@gmail.com>
2024-02-07 11:44:57 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './components/Nav';
+
+const App = () => {
+  //aquí escribimos código js
+  return (
+    <>
+    <div>App</div>
+    <Nav/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <nav class="menu">
+ <ul>
+    <li><a href="#">Inicio</a></li>
+    <li><a href="#">Sobre nosotros</a></li>
+    <li><a href="#">Contacto</a></li>
+  </ul>
+</nav>
+    </nav>
+  )
+}
+
+export default Nav
